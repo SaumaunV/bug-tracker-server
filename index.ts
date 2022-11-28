8135c133518a7eab7405b86bef05189d94372eb2
Saumaun <svahedipour1@gmail.com>
2022-11-27 19:16:36 -0600

    created express server with graphql

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+require('dotenv').config();
+
+
+const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+`);
+
+const root = {
+  hello: () => {
+    return "Hello!";
+  }
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
