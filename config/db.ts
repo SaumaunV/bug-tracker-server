77e1c1aaf64e604fc99dc043a4a8d0cb3abfe4d0
Saumaun <svahedipour1@gmail.com>
2022-12-30 23:13:24 -0600

    Updated database config file

--- config/db.ts
+++ config/db.ts
@@ -2,9 +2,6 @@ import { Pool } from "pg";
 require("dotenv").config();
 
 export const pool = new Pool({
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  connectionString: process.env.DATABASE_URL
 });
 
