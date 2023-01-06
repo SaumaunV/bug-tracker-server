fa6f8d9a48eea8ae364db71f98132e53b5c99ffe
Saumaun <svahedipour1@gmail.com>
2023-01-05 23:38:31 -0600

    index
    Initialized passport and express-session.
    Added routes for user register, login, and getUser.

--- index.ts
+++ index.ts
@@ -1,15 +1,38 @@
-import express from "express";
+import express, { Request } from "express";
 import { graphqlHTTP } from "express-graphql";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from './schema/type-defs';
 import { resolvers } from "./schema/resolvers";
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import session from 'express-session';
+import { pool } from './config/db';
+import passportStrategy from './config/passport';
+import connectPgSimple from "connect-pg-simple";
+
+const pgSession: typeof connectPgSimple.PGStore = require("connect-pg-simple")(session);
+require("dotenv").config();
+
+interface RegisterRequest extends Request {
+  body: {
+    username: string;
+    email: string;
+    password: string;
+  };
+}
+
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
 
-app.use(cors());
+app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 
 app.use(
   "/graphql",
@@ -18,4 +41,94 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(
+  session({
+    store: new pgSession({ pool: pool }),
+    secret: process.env.SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1209600000 },
+  })
+);
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passportStrategy(passport);
+
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if(err) throw err;
+    if(!user) res.json(null);
+    else {
+      req.logIn(user, (err) => {
+        if(err) return next(err);
+        console.log(req.user);
+        const {id, username, email, role} = req.user!
+        res.send({id, username, email, role});
+      })
+    }
+  })(req, res, next);
+});
+
+app.post("/register", async (req: RegisterRequest, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  try {
+    const client = await pool.connect();
+    const checkUser = await client.query(
+      "SELECT * FROM users where email= $1 or username= $2",
+      [email, username]
+    );
+    console.log(checkUser.rows[0]);
+    if(checkUser.rows[0]) res.send({message: 'User already exists'});
+    else{
+      await client.query(
+        "INSERT INTO users(username, email, password) VALUES($1, $2, $3);",
+        [username, email, hashedPassword]
+      );
+      client.release();
+      passport.authenticate("local", (err, user, info) => {
+        if (err) throw err;
+        if (!user) res.json(null);
+        else {
+          req.logIn(user, (err) => {
+            if (err) return next(err);
+            console.log(req.user);
+            const { id, username, email, role } = req.user!;
+            res.send({ id, username, email, role });
+            res.send(req.user);
+          });
+        }
+      })(req, res, next);
+    }      
+  } catch (error) {
+    res.send({message: 'An error has occurred'});
+  }
+});
+app.post("/logout", (req, res, next) => {
+  
+  const sessionStore = req.sessionStore;
+  sessionStore.destroy(req.sessionID, (error) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log('error 2');
+    req.logOut((err) => next(err));
+    res.clearCookie('connect.sid');
+    res.json({ success: true });
+  });
+});
+app.get("/user", (req, res) => {
+  if(req.user){
+    const { id, username, email, role } = req.user;
+    res.send({id, username, email, role});
+  }
+  else res.json(null);
+});
+
+
 app.listen(4000);
